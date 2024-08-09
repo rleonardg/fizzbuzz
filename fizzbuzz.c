ac7ae6c111131690491c360f25a45d29c3490925
rleonardg <maycolgabriel2002@gmail.com>
2024-08-09 19:06:52 +0200

    Add files via upload

---
+++ fizzbuzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int     i;
+    
+    i = 0;
+    while (++i <= 100)
+    {
+        if (i % 3 == 0 && i % 5 != 0)
+        {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0 && i % 3 != 0)
+        {
+            printf("Buzz\n");
+        }
+        else if (i % 5 == 0 && i % 3 == 0)
+        {
+            printf("FizzBuzz\n");
+        }
+        else
+        {
+            printf("%d\n", i);
+        }
+    }
+}
